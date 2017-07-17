251cbda8d943398540521a827982f6447ebf7dc7
Valdemar <xavierx95@gmail.com>
2017-07-17 22:51:35 +0300

    splahs

---
+++ app/src/main/java/com/vardemin/vcity/SplashActivity.java
@@ -0,0 +1,10 @@
+package com.vardemin.vcity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xavie on 17.07.2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+}

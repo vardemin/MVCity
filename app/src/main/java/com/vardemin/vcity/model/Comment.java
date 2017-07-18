aa90bcded4b8d49ff60b5becb91c16de97910091
Valdemar <xavierx95@gmail.com>
2017-07-19 00:05:40 +0300

    bd

--- app/src/main/java/com/vardemin/vcity/model/Comment.java
+++ app/src/main/java/com/vardemin/vcity/model/Comment.java
@@ -1,10 +1,16 @@
 package com.vardemin.vcity.model;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
-/**
- * Created by SimUser on 18.07.2017.
- */
 
 public class Comment extends RealmObject {
+    @PrimaryKey
+    private String id;
+    private String name;
+    private String email;
+    private int age;
+    private boolean sex;
+    private String avatar;
+    
 }

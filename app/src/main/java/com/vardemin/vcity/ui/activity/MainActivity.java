2ac97e0d5b4469b45e7255ea87c629092a6069ac
Valdemar <xavierx95@gmail.com>
2017-08-21 07:39:52 +0300

    added maps

--- app/src/main/java/com/vardemin/vcity/ui/activity/MainActivity.java
+++ app/src/main/java/com/vardemin/vcity/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.vardemin.vcity.ui.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.ncapdevi.fragnav.FragNavController;
 import com.vardemin.vcity.R;
 
 
@@ -10,12 +12,18 @@ import com.vardemin.vcity.R;
  * Created by xavie on 30.07.2017.
  */
 
-public class MainActivity extends MvpAppCompatActivity {
+public class MainActivity extends MvpAppCompatActivity implements FragNavController.RootFragmentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //FragNavController.newBuilder(savedInstanceState, getSupportFragmentManager(), R.id.container);
 
+    }
+
+    @Override
+    public Fragment getRootFragment(int i) {
+        return null;
     }
 }

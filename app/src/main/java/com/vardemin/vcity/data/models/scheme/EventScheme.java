4fa85c8f0f6b0195ab8c8481ba3b1c3d7b366199
Valdemar <xavierx95@gmail.com>
2017-10-03 23:36:09 +0300

    Icons, style, strings, layouts

--- app/src/main/java/com/vardemin/vcity/data/models/scheme/EventScheme.java
+++ app/src/main/java/com/vardemin/vcity/data/models/scheme/EventScheme.java
@@ -9,7 +9,7 @@ import java.util.List;
  * Created by xavie on 29.09.2017.
  */
 
-public class EventScheme {
+public class EventScheme{
     private String id;
     private String name;
     private String description;
@@ -17,4 +17,44 @@ public class EventScheme {
     private Date occuredAt;
     private List<PhotoScheme> photos;
     private List<Float> location;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public UserScheme getUser() {
+        return user;
+    }
+
+    public Date getOccuredAt() {
+        return occuredAt;
+    }
+
+    public List<PhotoScheme> getPhotos() {
+        return photos;
+    }
+
+    public List<Float> getLocation() {
+        return location;
+    }
+
+    public void setUser(UserScheme user) {
+        this.user = user;
+    }
+
+    public void setPhotos(List<PhotoScheme> photos) {
+        this.photos = photos;
+    }
+
+    public void setLocation(List<Float> location) {
+        this.location = location;
+    }
 }
--- app/src/main/java/com/vardemin/vcity/mvp/presenters/EventPresenter.java
+++ app/src/main/java/com/vardemin/vcity/mvp/presenters/EventPresenter.java
@@ -1,13 +1,30 @@
 package com.vardemin.vcity.mvp.presenters;
 
+import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
+import com.vardemin.vcity.App;
+import com.vardemin.vcity.mvp.repositories.local.ILocalDataRepository;
+import com.vardemin.vcity.mvp.repositories.remote.IRemoteDataRepository;
 import com.vardemin.vcity.mvp.views.EventView;
+
+import javax.inject.Inject;
 
 /**
  * Created by xavie on 19.07.2017.
  */
+@InjectViewState
 public class EventPresenter extends MvpPresenter<EventView> {
+    @Inject
+    ILocalDataRepository localDataRepository;
+    @Inject
+    IRemoteDataRepository remoteDataRepository;
+
+    public EventPresenter() {
+        App.getApplicationComponent().inject(this);
+    }
+
     void addEvent(String name, String description, String date, boolean useCurrentLocation) {
 
+        //remoteDataRepository.emit("events::create",);
     }
 }
